a99b238037efde7101102093f1855df9ee1f7763
Anderson M. Guimarães <anderson.m.guimaraes@icloud.com>
2024-08-23 14:24:45 -0300

    exercicio

--- aula06/exercicios/ex13.py
+++ aula06/exercicios/ex13.py
@@ -6,30 +6,15 @@ print('-='*60)
 
 mes = int(input('Escolha o mês correspondente de 1 a 12:\n'))
 
-match mes:
-    case 1:
-        print('Você escolheu "JANEIRO"')
-    case 2:
-        print('Você escolheu "FEVEREIRO"')
-    case 3:
-        print('Você escolheu "MARÇO"')
-    case 4:
-        print('Você escolheu "ABRIL"')
-    case 5:
-        print('Você escolheu "MAIO"')
-    case 6:
-        print('Você escolheu "JUNHO"')
-    case 7:
-        print('Você escolheu "JULHO"')
-    case 8:
-        print('Você escolheu "AGOSTO"')
-    case 9:
-        print('Você escolheu "SETEMBRO"')
-    case 10:
-        print('Você escolheu "OUTUBRO"')
-    case 11:
-        print('Você escolheu "NOVEMBRO"')
-    case 12:
-        print('Você escolheu "DEZEMBRO"')
-    case _:
-        print('Você escolheu um valor inválido.')
+while mes not in range(1,13):
+    print('O mês digitado é inválido')
+    break
+while mes in range(1,13):
+    if 3 <= mes <= 5:
+        print(f'O mês de {mes} é outono')
+    elif 6 <= mes <= 8:
+        print(f'O mês de {mes} é inverno')
+    elif 9 <= mes <= 11:
+        print(f'O mês de {mes} é primavera')
+    else:
+        print(f'O mês de {mes} é verão')
---
+++ aula06/exercicios/ex28.py
@@ -0,0 +1,15 @@
+# Escreva um programa que peça ao usuário para inserir uma palavra e verifique se ela tem mais de 5 letras.
+
+print('-='*60)
+print(f'("TAMANHO DA PALAVRA":^120)')
+print('-='*60)
+
+palavra = str(input('Digite uma palavra:\n'))
+
+if len(palavra) > 5:
+    print(f'{palavra} tem mais de 5 letras')
+elif len(palavra) == 5:
+    print(f'{palavra} tem 5 letras')
+else:
+    print(f'{palavra} tem menos de 5 letras')
+
