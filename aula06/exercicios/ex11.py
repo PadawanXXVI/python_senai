bcdb91c296e06035ac2137f7c836f72311ed0541
Anderson de Matos Guimarães <anderson.m.guimaraes@icloud.com>
2024-08-23 09:04:23 -0300

    exercício 11

---
+++ aula06/exercicios/ex11.py
@@ -0,0 +1,6 @@
+# Escreva um programa que solicite ao usuário para digitar seu nome e, em seguida, exiba uma mensagem de boas-vindas personalizada.
+print()
+
+usuario = str(input('Informe o seu nome:\n'))
+
+print(f'Olá, {usuario}! Seja bem vindo! Tenha um ótimo dia!')
