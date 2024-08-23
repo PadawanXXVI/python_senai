d6e8b9ced3a1f4173d02c50456e43a7b1bd08821
Anderson de Matos Guimarães <anderson.m.guimaraes@icloud.com>
2024-08-23 07:55:15 -0300

    exercício 4

--- aula06/exercicios/ex04.py
+++ aula06/exercicios/ex04.py
@@ -3,3 +3,17 @@
 print('-='*60)
 print(f'{"SISTEMA DE ESCOLHA DE CORES":^120}')
 print('-='*60)
+
+cor = int(input('Selecione uma cor: 1 - vermelho; 2 - verde; 3 - azul:\n'))
+if cor == 1:
+    print('A cor escolhida foi "vermelho"')
+elif cor == 2:
+    print('A cor escolhida foi "verde"')
+elif cor == 3:
+    print('A cor escolhida foi "azul"')
+else:
+    print('Número inválido')
+
+print('-='*60)
+print(f'{'FIM DO PROGRAMA':^120}')
+print('-='*60)
