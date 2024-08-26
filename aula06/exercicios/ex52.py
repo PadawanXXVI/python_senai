681fd71900767362b40a7572b1cd19f4dccf2e39
Anderson de Matos Guimarães <anderson.m.guimaraes@icloud.com>
2024-08-26 11:09:58 -0300

    atualização

--- aula06/exercicios/ex52.py
+++ aula06/exercicios/ex52.py
@@ -1,7 +1,8 @@
 # Desenvolva um algoritmo que solicite ao usuário uma senha e continue pedindo até que a senha correta "1234" seja digitada.
-senha = str(input('Informe a senha:\n'))
-while senha == '1234':
-    print("Senha válida!")
-    break
+senha_digitada = input("Digite a senha: ")
 
+while senha_digitada != '1234':
+    print("Senha incorreta. Tente novamente.")
+    senha_digitada = input("Digite a senha: ")
 
+print("Acesso permitido!")
