1f80b17fd00e4a6838fa7cb8d88a27577934b98c
Anderson de Matos Guimarães <anderson.m.guimaraes@icloud.com>
2024-08-26 10:21:16 -0300

    exercício 48

---
+++ aula06/exercicios/ex48.py
@@ -0,0 +1,5 @@
+# Escreva um algoritmo que solicite ao usuário uma palavra e exiba cada letra da palavra em uma linha separada.
+palavra = str(input('Digite uma palavra:\n'))
+for letra in palavra:
+    print(letra)
+    
