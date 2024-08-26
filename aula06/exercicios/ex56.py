70808a40545843b85b27a09571f748173ab29f31
Anderson M. Guimarães <anderson.m.guimaraes@icloud.com>
2024-08-26 16:41:57 -0300

    atualização

---
+++ aula06/exercicios/ex56.py
@@ -0,0 +1,2 @@
+# Escreva um programa que pergunte ao usuário quantas vezes ele quer que uma mensagem seja exibida, e utilize um laço while para exibir a mensagem a quantidade de vezes desejada.
+mensagem = str(input('Digite a mensagen:\n'))
