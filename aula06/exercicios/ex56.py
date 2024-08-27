9d722dc5fc908a927702107ce55205c3daca6d1e
Anderson de Matos Guimarães <anderson.m.guimaraes@icloud.com>
2024-08-27 07:59:40 -0300

    exercício 56

--- aula06/exercicios/ex56.py
+++ aula06/exercicios/ex56.py
@@ -1,2 +1,15 @@
 # Escreva um programa que pergunte ao usuário quantas vezes ele quer que uma mensagem seja exibida, e utilize um laço while para exibir a mensagem a quantidade de vezes desejada.
 mensagem = str(input('Digite a mensagen:\n'))
+repete = int(input('Informe quantas vezes você quer que a mensagem seja repetida [0 para sair]:\n'))
+contador = 0
+print('-='*60)
+while contador < repete:
+    print(mensagem)
+    contador += 1
+
+print('-='*60)
+print('FIM DA EXECUÇÃO')
+print('-='*60)
+
+
+    
