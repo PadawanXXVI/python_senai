1a953732ff0004719bf8e8737cf5cb0e5f4eeb0d
Anderson de Matos Guimarães <anderson.m.guimaraes@icloud.com>
2024-08-27 09:02:17 -0300

    exercício 32

--- aula06/exercicios/ex32.py
+++ aula06/exercicios/ex32.py
@@ -1,1 +1,13 @@
 # pular esta
+
+palindromo = 'a torre da derrota'
+palindromo2 = palindromo.strip()
+if palindromo == palindromo[::-1]:
+    print('É palíndromo')
+else:
+    print('Não é palíndromo')
+
+if palindromo == palindromo2:
+    print('É palíndromo')
+else:
+    print('Não é palíndromo')
