1815fbd80a6601f5db5a44c12a17261d7caca38d
Anderson M. Guimarães <anderson.m.guimaraes@icloud.com>
2024-08-30 17:35:58 -0300

    jogo da velha

---
+++ exercicios2/ex067/jogo_velha.py
@@ -0,0 +1,21 @@
+from pickle import FALSE
+import velha_funcoes as jv
+
+jogador = 'O'
+vencedor = False
+
+while vencedor == False:
+    jv.desenhar_tabuleiro()
+
+    jogada = int(input('Onde deseja jogar [0 a 8]? '))
+
+    jv.jogar(jogada, jogador)
+
+    jv.desenhar_tabuleiro()
+
+    jogador = jv.troca_jogador(jogador)
+
+    vencedor = jv.verifica_vitoria()
+
+jogador = jv.troca_jogador(jogador)
+print(f'O jogador {jogador} venceu!')
